7c69bd45f763db945afe7627eb41895d7d3566e3
Michał Kopta <michal.kopta@schibsted.com>
2022-08-31 14:16:41 +0200

    Add Migrations and Database

---
+++ EscaperoomBookingAPI.Infrastructure/Migrations/20220831072834_InitialCreate.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EscaperoomBookingAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Summary",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    BookingVariant = table.Column<int>(type: "INTEGER", nullable: false),
+                    Price = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Summary", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookingDetails",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    SelectedRoom = table.Column<int>(type: "INTEGER", nullable: false),
+                    VisitDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    NumberOfPeople = table.Column<int>(type: "INTEGER", nullable: false),
+                    SummaryReference = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookingDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BookingDetails_Summary_SummaryReference",
+                        column: x => x.SummaryReference,
+                        principalTable: "Summary",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerDetails",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    PhoneNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    OtherInfo = table.Column<string>(type: "TEXT", nullable: true),
+                    SummaryReference = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CustomerDetails_Summary_SummaryReference",
+                        column: x => x.SummaryReference,
+                        principalTable: "Summary",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookingDetails_SummaryReference",
+                table: "BookingDetails",
+                column: "SummaryReference",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerDetails_SummaryReference",
+                table: "CustomerDetails",
+                column: "SummaryReference",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookingDetails");
+
+            migrationBuilder.DropTable(
+                name: "CustomerDetails");
+
+            migrationBuilder.DropTable(
+                name: "Summary");
+        }
+    }
+}
